e14c3d14258c37dd633186da5ae90aa81d816582
Theodor Vararu <theo@vararu.org>
2015-10-10 15:53:29 +0100

    Create player component.

--- react/components/Test.jsx
+++ react/components/Test.jsx
@@ -1,25 +1,39 @@
-import { Component } from 'react'
+import { Component, PropTypes } from 'react'
 const LoginButtons = BlazeToReact('loginButtons')
+
+class Player extends Component {
+  static displayName = 'Player'
+
+  static propTypes = {
+    songId: PropTypes.string.isRequired
+  }
+
+  render () {
+    console.log(this.props.songId)
+    return <audio controls='controls'>
+      <source src={ `/listen/song/${this.props.songId}` } type='audio/mpeg' />
+    </audio>
+  }
+}
 
 export default class Test extends Component {
   static displayName = 'Test'
 
-  constructor (props) {
-    super(props)
-    require('./Test.css')
+  state = {
+    songId: '42carats'
+  }
+
+  changeSong = () => {
+    console.log('changeSong')
+    this.setState({ songId: 'mistertenbelow' })
   }
 
   render () {
-    // const userId = Meteor.user()._id
-    const songId = '42carats'
-    return (
-      <div>
-        <LoginButtons />
-        <br />
-        <audio controls='controls'>
-          <source src={ `/listen/song/${songId}` } type='audio/mpeg' />
-        </audio>
-      </div>
-    )
+    return <div>
+      <LoginButtons />
+      <br />
+      <button onClick={ this.changeSong }>Change song</button>
+      <Player songId={ this.state.songId } />
+    </div>
   }
 }
