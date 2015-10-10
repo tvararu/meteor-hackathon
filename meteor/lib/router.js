f4721c910863757c735d2ad6e6e353931af5508b
Theodor Vararu <theo@vararu.org>
2015-10-10 14:46:33 +0100

    Attempting to remotely close a node stream.

--- meteor/lib/router.js
+++ meteor/lib/router.js
@@ -2,16 +2,36 @@ Router.route('/', function () {
   this.render(null)
 })
 
+let WRITABLE = null
+let READABLE = null
+
 Router.route('/listen', function () {
   const fs = Npm.require('fs')
-  const fullFilePath = '/Users/tvararu/github/node-mp3-test/song.mp3'
-  const stat = fs.statSync(fullFilePath)
+  const fullFilePath = '/Users/tvararu/Music/Bones - Banshee/07 MisterTenBelow.mp3'
 
   this.response.writeHead(200, {
-    'Content-Type': 'audio/mpeg',
-    'Content-Length': stat.size
+    'Content-Type': 'audio/mpeg'
   })
 
+  WRITABLE = this.response
   const readStream = fs.createReadStream(fullFilePath)
-  readStream.pipe(this.response)
+  READABLE = readStream
+  READABLE.pipe(WRITABLE)
+}, { where: 'server' })
+
+Router.route('/change', function () {
+  console.log('Change')
+  const fs = Npm.require('fs')
+  const fullFilePath = '/Users/tvararu/Music/Bones - Banshee/08 42Carats.mp3'
+  const readStream = fs.createReadStream(fullFilePath)
+  READABLE.unpipe(WRITABLE)
+  WRITABLE.end()
+  this.response.end()
+}, { where: 'server' })
+
+Router.route('/stop', function () {
+  console.log('Stop', WRITABLE)
+  READABLE.unpipe(WRITABLE)
+  WRITABLE.end()
+  this.response.end()
 }, { where: 'server' })
